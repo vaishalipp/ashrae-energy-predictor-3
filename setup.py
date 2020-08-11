81bb156e8741103254c5b144a58ea8fda2014840
vaishalipp <39810589+vaishalipp@users.noreply.github.com>
2020-08-11 18:13:46 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the second capstone under springboard Datascience bootcamp based on a kaggle competition',
+    author='Vaishali Prabhu',
+    license='',
+)
